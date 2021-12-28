d96cfa73f740a831e117ba79a4bbd3c7dee2c7c0
Peng Chen <pchen@thoughtworks.com>
2021-12-28 18:01:23 +0800

    add mockito for kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 }
 
 dependencies {
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
